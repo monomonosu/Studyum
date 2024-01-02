55287511e6df2dfd10a066d2ab344358db74a42f
monomonosu <pojinyan@yahoo.co.jp>
2024-01-02 19:22:22 +0900

    カスタムテーマ作成

---
+++ src/@types/createPalette.d.ts
@@ -0,0 +1,9 @@
+declare module "@mui/material/styles/createPalette" {
+  interface CustomPaletteColorOptions {
+    main: string;
+    light?: string;
+    dark?: string;
+    contrastText?: string;
+    background?: string;
+  }
+}
---
+++ src/utils/themes/muiTheme.ts
@@ -0,0 +1,22 @@
+import { createTheme } from "@mui/material/styles";
+import { blue, green, red } from "@mui/material/colors";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[600],
+      light: blue[300],
+      dark: blue[800],
+    },
+    success: {
+      main: green[600],
+      light: green[300],
+      dark: green[800],
+    },
+    error: {
+      main: red[600],
+      light: red[300],
+      dark: red[800],
+    },
+  },
+});
