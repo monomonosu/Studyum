0f9c914dc3ca73444d66fb96931539999615b489
monomonosu <pojinyan@yahoo.co.jp>
2024-01-02 20:58:31 +0900

    reset.css上書き用グローバルCSS追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/reset.css";
+import "@/styles/globals.css";
 import { Providers } from "@/app/providers";
 import HeaderMain from "@/components/Layouts/HeaderMain";
 
