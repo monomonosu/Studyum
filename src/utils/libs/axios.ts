7b573fec958871417434bc430829d0ca077ccec0
monomonosu <pojinyan@yahoo.co.jp>
2024-05-12 23:51:18 +0900

    axiosのベースURL設定。デプロイ環境でAPIを叩けるかどうかのテスト。

---
+++ src/utils/libs/axios.ts
@@ -0,0 +1,8 @@
+import Axios from "axios";
+
+export const axiosClient = Axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
