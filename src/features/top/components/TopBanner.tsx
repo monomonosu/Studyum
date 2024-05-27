4a185893ad5bebaf5c7fb552a26dfb213a56b204
monomonosu <pojinyan@yahoo.co.jp>
2024-05-28 03:02:38 +0900

    stylelintで整形。エラー箇所の解消。

--- src/features/top/components/TopBanner.tsx
+++ src/features/top/components/TopBanner.tsx
@@ -1,33 +1,23 @@
-import Link from "next/link";
-import { BasicButton } from "@/components/Buttons/BasicButton";
-import style from "@/styles/features/top/components/top_banner.module.scss";
+import Link from 'next/link';
+import { BasicButton } from '@/components/Buttons/BasicButton';
+import style from '@/styles/features/top/components/top_banner.module.scss';
 
 export default function TopBanner() {
   return (
     <>
-      <div className={style.banner_container}>
-        <div className={style.banner_inner}>
+      <div className={style['banner-container']}>
+        <div className={style['banner-inner']}>
           <p>
             Studyumは学ぶ人同士を繋ぐプラットフォームです。
             <br />
             気軽にもくもく会を開いてみましょう。
           </p>
-          <div className={style.custom_wrapper}>
-            <Link className={style.custom_link} href="/info">
-              <BasicButton
-                text="Studyumについて"
-                color="success"
-                variant="contained"
-                className="-shadow"
-              />
+          <div className={style['custom-wrapper']}>
+            <Link className={style['custom-link']} href='/info'>
+              <BasicButton text='Studyumについて' color='success' variant='contained' className='-shadow' />
             </Link>
-            <Link className={style.custom_link} href="/info/howto">
-              <BasicButton
-                text="Studyumの使い方"
-                color="success"
-                variant="contained"
-                className="-shadow"
-              />
+            <Link className={style['custom-link']} href='/info/howto'>
+              <BasicButton text='Studyumの使い方' color='success' variant='contained' className='-shadow' />
             </Link>
           </div>
         </div>
