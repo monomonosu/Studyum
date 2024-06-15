294a8d12ffeade52c798dc8af705af0c966a5d3d
monomonosu <pojinyan@yahoo.co.jp>
2024-06-15 23:51:58 +0900

    バリデーションスキーマ生成用utils作成

---
+++ src/utils/constants/validationMassages.ts
@@ -0,0 +1,11 @@
+/**
+ * @name 入力必須
+ * @return 〇〇は必須項目です。
+ */
+export const REQUIRED_TEXT = (name: string) => `${name}は必須項目です。`
+
+/**
+ * @name URL形式
+ * @return URL形式で入力してください。
+ */
+export const URL_FORMAT_TEXT = () => `URL形式で入力してください。`
