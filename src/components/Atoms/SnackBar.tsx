f2ee136c0b82de5edacdadb9e57a7822e1f7f84b
monomonosu <pojinyan@yahoo.co.jp>
2024-09-01 18:35:22 +0900

    ディレクトリ移動。hooks作成。stories作成。

--- src/components/Atoms/SnackBar.tsx
+++ src/components/Atoms/SnackBar.tsx
@@ -1,5 +1,7 @@
 import { SnackbarType } from '@/utils/libs/stores/snackbar'
 import { Alert, Snackbar } from '@mui/material'
+import DoDisturbAltIcon from '@mui/icons-material/DoDisturbAlt'
+import TaskAltIcon from '@mui/icons-material/TaskAlt'
 
 type Props = {
   snackbar: SnackbarType
@@ -19,6 +21,7 @@ export const SnackBar = ({ snackbar, onClose }: Props) => {
       onClose={onClose}
     >
       <Alert
+        icon={status === 200 ? <TaskAltIcon /> : <DoDisturbAltIcon />}
         severity={status === 200 ? 'success' : 'error'}
         variant='filled'
         sx={{ width: '100%' }}
